efa1e5b8b80d6d70d4dbc5cdac0ee8e2ea1c13d2
KD <kdpembile@securitybank.com.ph>
2024-06-11 11:47:59 +0800

    Initial commit

---
+++ demo-kafka-streams/src/main/java/com/kentisthebest/demo_kafka_streams/DemoKafkaStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.kentisthebest.demo_kafka_streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKafkaStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKafkaStreamsApplication.class, args);
+	}
+
+}
